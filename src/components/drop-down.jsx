76b876eda0d59101149d33b50cfff6feaa865bc6
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-08-24 03:02:44 -0300

    feat: create DropDown component
    This commit create a custom component for represeting a select html.

---
+++ src/components/drop-down.jsx
@@ -0,0 +1,24 @@
+export default function DropDown({ id, name, label, options, onChange }) {
+  return (
+    <div className="flex flex-col gap-y-[8px]">
+      <label htmlFor={id} className="text-[22px] pl-[2px]">
+        {label}
+      </label>
+      <select
+        id={id}
+        name={name}
+        onChange={onChange}
+        className="w-[350px] h-[50px] text-[22px] pl-[16px] rounded shadow focus:outline-0"
+      >
+        <option value={null} defaultChecked>
+          Select
+        </option>
+        {options.map((option, index) => (
+          <option key={index} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
