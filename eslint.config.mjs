17cf4a4aac0f390fa6734b2b9315666af1e29bf5
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-08-24 00:05:26 -0300

    chore: change eslint config
    This commit implements the integration with prettier on eslint config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,24 @@
 import globals from "globals";
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   pluginReact.configs.flat.recommended,
+  configPrettier,
   {
     rules: {
       "no-unused-vars": "warn",
       quotes: ["error", "double"],
       semi: ["error", "always"],
-      indent: ["error", 2],
       "prefer-const": "error",
       "no-console": "warn",
       eqeqeq: ["error", "always"],
-      "max-len": ["warn", { code: 120 }],
+      "prettier/prettier": "error",
     },
   },
 ];
