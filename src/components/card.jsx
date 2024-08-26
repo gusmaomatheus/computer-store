4cb39847ff2c5f1fcbd0640d6d64e8a2147bbfd5
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-08-26 17:40:21 -0300

    style: change way of the image is show on Card component
    This commit adjusts the image to fit better within the container.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,11 +7,9 @@ export default function Card({
   return (
     <div className="w-[400px] h-[320px] flex flex-col rounded-md shadow">
       <div className="h-[40%] flex flex-col items-center justify-center bg-green-50 border-b-2 border-gray-300">
-        <img
-          src={brandLogo}
-          alt="Brand logo of the product"
-          className="w-[100px] h-[100px] rounded-full border-[1px] border-gray-300"
-        />
+        <div className="w-[100px] h-[100px] flex items-center justify-center overflow-hidden border-[1px] border-gray-300 rounded-full">
+          <img src={brandLogo} alt="Brand logo of the product" className="max-w-full max-h-full" />
+        </div>
       </div>
       <div className="flex flex-col mt-[40px] gap-y-[5px]">
         <p className="text-[22px] text-center">{name}</p>
